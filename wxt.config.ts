faea4cebbb138dba7ed58e2e8964eeeebf8e5c43
technohippies <hi@x.technohippies>
2025-04-16 15:21:50 +0200

    Fixing shadcn installation

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "wxt";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  vite: () => ({
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }),
   webExt: {
     startUrls: ["https://google.com"],
   },
